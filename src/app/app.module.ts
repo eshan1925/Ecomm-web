a688fa4de8e2f67aba2f72ccf349a06236d4a87f
eshan1925 <eshan1925@gmail.com>
2021-10-14 15:20:55 +0530

    Added Header section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http'; 
+import { MatIconModule } from '@angular/material/icon';
+import { HeaderComponent } from './COMPONENTS/header/header.component';
+import { HomeComponent } from './PAGES/home/home.component';
+import { FooterComponent } from './COMPONENTS/footer/footer.component';
+import { CheckoutComponent } from './PAGES/checkout/checkout.component';
+import { LoginComponent } from './PAGES/login/login.component';
+import { BannerComponent } from './COMPONENTS/banner/banner.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent,
+    CheckoutComponent,
+    LoginComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
